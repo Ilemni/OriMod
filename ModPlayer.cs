440159708bfc65a4d9e4a9de7159dae38ff9cb46
TwiliChaos <zundisplayed@gmail.com>
2019-06-20 23:59:05 -0700

    Move PlayerLayers to separate file

--- ModPlayer.cs
+++ ModPlayer.cs
@@ -230,14 +230,14 @@ namespace OriMod {
     internal List<int> UnassignedTiles;
 
     // Trail variables, for the trails Ori creates
-    private List<Trail> Trails;
-    private int TrailIndex = 0;
+    internal List<Trail> Trails;
+    internal int TrailIndex = 0;
     private int TeatherTrailTimer = 0;
 
     // Animation Variables
     internal const int SpriteWidth = 128;
     internal const int SpriteHeight = 128;
-    private Point AnimFrame;
+    internal Point AnimFrame;
     /// <summary>
     /// The current sprite tile of the player in Ori state
     /// 
@@ -691,6 +691,35 @@ namespace OriMod {
       Vector2 tilepos = new Vector2(pos.ToTileCoordinates().X, pos.ToTileCoordinates().Y);
       return Main.tile[(int)tilepos.X, (int)tilepos.Y];
     }
+    private void GetMaterial(Tile tile) {
+      if (GrassFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Grass";
+      }
+      else if (LightDarkFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "LightDark";
+      }
+      else if (MushroomFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Mushroom";
+      }
+      else if (RockFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Rock";
+      }
+      else if (SandFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Sand";
+      }
+      else if (SnowFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Snow";
+      }
+      else if (SpiritTreeRockFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "SpiritTreeRock";
+      }
+      else if (SpiritTreeWoodFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "SpiritTreeWood";
+      }
+      else if (WoodFloorMaterials.Contains(tile.type)) {
+        FloorMaterial = "Wood";
+      }
+    }
     private void TestStepMaterial(Player player) { // oh yeah good luck understanding what this is
       Tile tile = GetTile(-12f, 4f);
       if (tile.liquid > 0f && tile.liquidType() == 0) {
@@ -704,64 +733,12 @@ namespace OriMod {
         else {
           tile = GetTile(-12f, 8);
           if (tile.active()) {
-            if (GrassFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Grass";
-            }
-            else if (LightDarkFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "LightDark";
-            }
-            else if (MushroomFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Mushroom";
-            }
-            else if (RockFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Rock";
-            }
-            else if (SandFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Sand";
-            }
-            else if (SnowFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Snow";
-            }
-            else if (SpiritTreeRockFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "SpiritTreeRock";
-            }
-            else if (SpiritTreeWoodFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "SpiritTreeWood";
-            }
-            else if (WoodFloorMaterials.Contains(tile.type)) {
-              FloorMaterial = "Wood";
-            }
+            GetMaterial(tile);
           }
           else {
             tile = GetTile(-12, 24);
             if (tile.active()) {
-              if (GrassFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Grass";
-              }
-              else if (LightDarkFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "LightDark";
-              }
-              else if (MushroomFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Mushroom";
-              }
-              else if (RockFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Rock";
-              }
-              else if (SandFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Sand";
-              }
-              else if (SnowFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Snow";
-              }
-              else if (SpiritTreeRockFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "SpiritTreeRock";
-              }
-              else if (SpiritTreeWoodFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "SpiritTreeWood";
-              }
-              else if (WoodFloorMaterials.Contains(tile.type)) {
-                FloorMaterial = "Wood";
-              }
+              GetMaterial(tile);
             }
             else {
               FloorMaterial = "Grass";
@@ -960,8 +937,8 @@ namespace OriMod {
     }
     public override void ModifyDrawLayers(List<PlayerLayer> layers) {
       if (bash.InUse && bash.Npc != null) {
-        layers.Insert(0, oriBashArrow);
-        oriBashArrow.visible = true;
+        layers.Insert(0, OriLayers.BashArrow);
+        OriLayers.BashArrow.visible = true;
       }
       if (OriSet || Transforming) {
         PlayerLayer.Skin.visible = false;
@@ -980,143 +957,25 @@ namespace OriMod {
         PlayerLayer.HandOffAcc.visible = false;
 
         if (Transforming && TransformTimer > 235) {
-          layers.Insert(0, oriTransformSprite);
-          oriTransformSprite.visible = true;
+          layers.Insert(0, OriLayers.TransformSprite);
+          OriLayers.TransformSprite.visible = true;
         }
         if (OriSet) {
-          layers.Insert(9, oriPlayerSprite);
-          layers.Insert(0, OriTrail);
+          layers.Insert(9, OriLayers.PlayerSprite);
+          layers.Insert(0, OriLayers.Trail);
           player.head = mod.GetEquipSlot("OriHead", EquipType.Head);
-          oriPlayerSprite.visible = (!player.dead && !player.invis);
-          OriTrail.visible = (!player.dead && !player.invis && !player.mount.Active);
+          OriLayers.PlayerSprite.visible = (!player.dead && !player.invis);
+          OriLayers.Trail.visible = (!player.dead && !player.invis && !player.mount.Active);
           if (glide.InUse) {
-            layers.Insert(0, oriFeatherSprite);
+            layers.Insert(0, OriLayers.FeatherSprite);
           }
         }
       }
       else {
-        OriTrail.visible = false;
-        oriPlayerSprite.visible = false;
+        OriLayers.Trail.visible = false;
+        OriLayers.PlayerSprite.visible = false;
       }
     }
-    private Vector2 Offset(int x=0, int y=0) {
-      Vector2 offset = new Vector2(x, y);
-      return offset;
-    }
-    internal readonly PlayerLayer OriTrail = new PlayerLayer("OriMod", "OriTrail", delegate (PlayerDrawInfo drawInfo) {
-      Player drawPlayer = drawInfo.drawPlayer;
-      Mod mod = ModLoader.GetMod("OriMod");
-      OriPlayer oPlayer = drawPlayer.GetModPlayer<OriPlayer>(mod);
-      for (int i = 0; i < 26; i++) {
-        Trail trail = oPlayer.Trails[i];
-        trail.Alpha -= 0.00002f;
-        if (trail.Alpha < 0) {
-          trail.Alpha = 0;
-        }
-      }
-      if (!drawPlayer.dead && !drawPlayer.invis) {
-        oPlayer.TrailIndex++;
-        if (oPlayer.TrailIndex > 25) {
-          oPlayer.TrailIndex = 0;
-        }
-        Trail trail = oPlayer.Trails[oPlayer.TrailIndex];
-        trail.Position = drawPlayer.Center;
-        trail.Frame = oPlayer.AnimFrame;
-        trail.Direction = drawPlayer.direction;
-        float alpha = drawPlayer.velocity.Length() * 0.002f;
-        if (alpha > 0.005f) alpha = 0.005f;
-        trail.Alpha = alpha;
-        trail.Rotation = oPlayer.AnimRads;
-        if (trail.Alpha > 104) {
-          trail.Alpha = 104;
-        }
-      }
-      for (int i = 0; i < 26; i++) {
-        Trail trail = oPlayer.Trails[i];
-        SpriteEffects effect = trail.Direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-
-        Color color = oPlayer.SpriteColor * trail.Alpha;
-        color.A = 0;
-        DrawData data = new DrawData(
-          mod.GetTexture("PlayerEffects/OriGlow"),
-          new Vector2(trail.Position.X - Main.screenPosition.X, trail.Position.Y - Main.screenPosition.Y),
-          new Rectangle(trail.X, trail.Y, OriPlayer.SpriteWidth, OriPlayer.SpriteHeight), color, trail.Rotation,
-          new Vector2(SpriteWidth / 2, SpriteHeight / 2 + 6), 1, effect, 0
-        );
-        data.position += oPlayer.Offset();
-        Main.playerDrawData.Add(data);
-      }
-    });
-    internal static void GetSpriteInfo(Player player, OriPlayer oPlayer, out Vector2 pos, out SpriteEffects effect, out Rectangle rect, out Vector2 orig) {
-      pos = new Vector2(player.Center.X - Main.screenPosition.X, player.Center.Y - Main.screenPosition.Y);
-      effect = player.direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-      rect = new Rectangle((int)oPlayer.AnimFrame.X, (int)oPlayer.AnimFrame.Y, SpriteWidth, SpriteHeight);
-      orig = new Vector2(SpriteWidth / 2, SpriteHeight / 2 + 6);
-    }
-    internal static readonly PlayerLayer oriPlayerSprite = new PlayerLayer("OriMod", "OriPlayer", delegate (PlayerDrawInfo drawInfo) {
-      Mod mod = ModLoader.GetMod("OriMod");
-      Player drawPlayer = drawInfo.drawPlayer;
-      OriPlayer oPlayer = drawPlayer.GetModPlayer<OriPlayer>(mod);
-      Texture2D spriteTexture = mod.GetTexture("PlayerEffects/OriPlayer");
-      Vector2 pos;
-      SpriteEffects effect;
-      Rectangle rect;
-      Vector2 orig;
-      GetSpriteInfo(drawPlayer, oPlayer, out pos, out effect, out rect, out orig);
-      
-      DrawData data = new DrawData(spriteTexture, pos, rect, oPlayer.SpriteColor, drawPlayer.direction * oPlayer.AnimRads, orig, 1, effect, 0);
-      data.position += oPlayer.Offset();
-      Main.playerDrawData.Add(data);
-    });
-    internal static readonly PlayerLayer oriTransformSprite = new PlayerLayer("OriMod", "OriTransform", delegate (PlayerDrawInfo drawInfo) {
-      Player drawPlayer = drawInfo.drawPlayer;
-      Mod mod = ModLoader.GetMod("OriMod");
-      OriPlayer oPlayer = drawPlayer.GetModPlayer<OriPlayer>(mod);
-      Vector2 position = drawPlayer.position;
-      Texture2D texture = mod.GetTexture("PlayerEffects/transform");
-      SpriteEffects effect = drawPlayer.direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-      float t = oPlayer.TransformTimer - 235;
-      int y = t > 390 ? 0 : t > 330 ? 1 : t > 270 ? 2 : t > 150 ? 3 : t > 110 ? 4 : t > 70 ? 5 : t > 30 ? 6 : 7;
-      
-      DrawData data = new DrawData(texture,
-        new Vector2(drawPlayer.position.X - Main.screenPosition.X + 10, drawPlayer.position.Y - Main.screenPosition.Y + 8),
-        new Rectangle(0, y * 76, 104, 76),
-        Color.White, drawPlayer.direction * oPlayer.AnimRads,
-        new Vector2(52, 38), 1, effect, 0);
-      data.position += oPlayer.Offset();
-      Main.playerDrawData.Add(data);
-    });
-    internal static readonly PlayerLayer oriBashArrow = new PlayerLayer("OriMod", "bashArrow", delegate (PlayerDrawInfo drawInfo) {
-      Player drawPlayer = drawInfo.drawPlayer;
-      Mod mod = ModLoader.GetMod("OriMod");
-      OriPlayer oPlayer = drawPlayer.GetModPlayer<OriPlayer>(mod);
-      Texture2D texture = mod.GetTexture("PlayerEffects/bashArrow");
-      SpriteEffects effect = SpriteEffects.None;
-      int y = oPlayer.bash.CurrDuration < 40 ? 0 : oPlayer.bash.CurrDuration < 50 ? 1 : 2;
-
-      DrawData data = new DrawData(texture,
-        new Vector2(oPlayer.bash.Npc.Center.X - Main.screenPosition.X, oPlayer.bash.Npc.Center.Y - Main.screenPosition.Y),
-        new Rectangle(0, y * 20, 152, 20),
-        Color.White, oPlayer.bash.Npc.AngleTo(Main.MouseWorld),
-        new Vector2(76, 10), 1, effect, 0);
-      Main.playerDrawData.Add(data);
-    });
-    internal static readonly PlayerLayer oriFeatherSprite = new PlayerLayer("OriMod", "Feather", delegate (PlayerDrawInfo drawInfo) {
-      Mod mod = ModLoader.GetMod("OriMod");
-      Player drawPlayer = drawInfo.drawPlayer;
-      OriPlayer oPlayer = drawPlayer.GetModPlayer<OriPlayer>(mod);
-      Texture2D spriteTexture = mod.GetTexture("PlayerEffects/Feather");
-      Vector2 pos;
-      SpriteEffects effect;
-      Rectangle rect;
-      Vector2 orig;
-      GetSpriteInfo(drawPlayer, oPlayer, out pos, out effect, out rect, out orig);
-      rect.X = 0;
-
-      DrawData data = new DrawData(spriteTexture, pos, rect, oPlayer.SpriteColor, drawPlayer.direction * oPlayer.AnimRads, orig, 1, effect, 0);
-      data.position += oPlayer.Offset();
-      Main.playerDrawData.Add(data);
-    });
     public override void ResetEffects() {
       if (TransformTimer > 0) {
         TransformTimer -= HasTransformedOnce ? RepeatedTransformRate : 1;
