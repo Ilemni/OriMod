bd3da0519ec0bbebf0cde361068f9985ebb30fbe
TwiliChaos <zundisplayed@gmail.com>
2019-08-06 19:44:08 -0700

    Show config default values in tooltip

--- ModConfig.cs
+++ ModConfig.cs
@@ -12,56 +12,58 @@ namespace OriMod {
     
     [Header("Gameplay")]
     
-    [Label("Ability Cooldowns"), Tooltip("Determines if abilities have cooldowns. Keep enabled for a more balanced experience.")]
+    [Label("Ability Cooldowns"), Tooltip("Determines if abilities have cooldowns. Keep enabled for a more balanced experience.\nDefault: true")]
     [DefaultValue(true)]
     public bool AbilityCooldowns;
     
-    [Label("Player Light"), Tooltip("Determines if your player faintly glows.")]
+    [Label("Player Light"), Tooltip("Determines if your player faintly glows.\nDefault: true")]
     [DefaultValue(true)]
     public bool PlayerLight;
     
-    [Label("Global Player Light"), Tooltip("Determines if all other players glow based on your config or their config.")]
+    [Label("Global Player Light"), Tooltip("Determines if all other players glow based on your config or their config.\nDefault: false")]
     [DefaultValue(false)]
     public bool GlobalPlayerLight;    
     
     [Header("Controls")]
     
-    [Label("Stomp Activation Delay"), Tooltip("Duration the Stomp key must be held until activating Stomp.")]
+    [Label("Stomp Activation Delay"), Tooltip("Duration the Stomp key must be held until activating Stomp.\nDefault: 0")]
     [DefaultValue(0)]
     public float StompHoldDownDelay = 0;
     
-    [Label("Soft Crouch"), Tooltip("Allows moving while crouched.")]
+    [Label("Soft Crouch"), Tooltip("Allows moving while crouched.\nDefault: false")]
     [DefaultValue(false)]
     public bool SoftCrouch;
     
-    [Label("Smooth Camera"), Tooltip("Smooths the camera when it moves.")]
+    [Label("Smooth Camera"), Tooltip("Smooths the camera when it moves.\nDefault: true")]
     [DefaultValue(true)]
     public bool SmoothCamera;
     
-    // [Label("Auto Burrow"), Tooltip("After burrowing, if holding the Burrow key, automatically re-enter Burrow as soon as possible.")]
+    // [Label("Auto Burrow"), Tooltip("After burrowing, if holding the Burrow key, automatically re-enter Burrow as soon as possible.\nDefault: false")]
+    // [DefaultValue(false)]
     [JsonIgnore] // Buggy implementation
     public bool AutoBurrow;
 
     [JsonIgnore]
     internal bool BurrowToMouse => BurrowControls == "Mouse";
     
-    [Label("Burrow Control"), Tooltip("Determines which controls you use while burrowing.")]
+    [Label("Burrow Control"), Tooltip("Determines which controls you use while burrowing.\nDefault: Mouse")]
     [DefaultValue("Mouse"), OptionStrings(new string[] {"WASD", "Mouse"})]
     public string BurrowControls;
     
     [Header("Aesthetics")]
 
-    [Label("Player Color"), Tooltip("The color of your Spirit Guardian.")]
+    [Label("Player Color"), Tooltip("The color of your Spirit Guardian.\nDefault: 210, 255, 255")]
     [DefaultValue(typeof(Color), "210, 255, 255, 255")]
     public Color PlayerColor;
     
-    [Label("Player Color (Secondary)"), Tooltip("The secondary color of your Spirit Guardian.")]
+    [Label("Player Color (Secondary)"), Tooltip("The secondary color of your Spirit Guardian.\nDefault: 0, 0, 0, 0")]
     [DefaultValue(typeof(Color), "0, 0, 0, 0")]
     public Color PlayerColorSecondary;
 
     public override void OnLoaded() {
       OriMod.ConfigClient = this;
     }
+
     [OnDeserialized]
     public void OnDeserializedMethod(StreamingContext stream) {
       PlayerColor.A = 255;
@@ -75,105 +77,106 @@ namespace OriMod {
 
     [Header("Soul Link")]
     
-    [Label("Cooldown"), Tooltip("Cooldown of Soul Link, in seconds.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Soul Link, in seconds.\nDefault: 30")]
     [DefaultValue(30), Range(0f, 150)]
-    internal float SoulLinkCooldown;
+    public float SoulLinkCooldown;
 
-    [Label("Charge Time"), Tooltip("How long it takes to place a Soul Link, in seconds.")]
+    [Label("Charge Time"), Tooltip("How long it takes to place a Soul Link, in seconds.\nDefault: 0.9")]
     [DefaultValue(0.9f), Range(0.05f, 10)]
-    internal float SoulLinkChargeRate;
+    public float SoulLinkChargeRate;
 
-    [Label("Respawn Time"), Tooltip("How long after dying it takes to respawn at a Soul Link, in seconds.")]
+    [Label("Respawn Time"), Tooltip("How long after dying it takes to respawn at a Soul Link, in seconds.\nDefault: 1.2")]
     [DefaultValue(1.2f), Range(1f, 10)]
-    internal float SoulLinkRespawnTime;
+    public float SoulLinkRespawnTime;
 
     [Header("Air Jump")]
 
-    [Label("Number of Jumps"), Tooltip("Amount of times the player can jump in the air.")]
+    [Label("Number of Jumps"), Tooltip("Amount of times the player can jump in the air.\nDefault: 2")]
     [DefaultValue(2)]
     public int AirJumpCount;
     
     [Header("Bash")]
 
-    [Label("Cooldown"), Tooltip("Cooldown of Bash, in seconds.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Bash, in seconds.\nDefault: 0.75")]
     [DefaultValue(0.75f), Range(0f, 20)]
     public float BashCooldown;
 
-    [Label("Strength"), Tooltip("How far Bashing pushes the player and NPCs.")]
+    [Label("Strength"), Tooltip("How far Bashing pushes the player and NPCs.\nDefault: 15")]
     [DefaultValue(15), Range(0f, 50)]
     public float BashStrength;
 
-    [Label("Range"), Tooltip("Furthest distance from the player a target can be. 1 tile = 16 units.")]
+    [Label("Range"), Tooltip("Furthest distance from the player a target can be. 1 tile = 16 units.\nDefault: 48")]
     [DefaultValue(48), Range(8f, 160)]
     public float BashRange;
 
     [Header("Stomp")]
 
-    [Label("Cooldown"), Tooltip("Cooldown of Stomp, in seconds.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Stomp, in seconds.\nDefault: 6")]
     [DefaultValue(6), Range(0f, 30)]
     public float StompCooldown;
 
-    [Label("Fall Speed"), Tooltip("How fast the player falls while Stomping.")]
+    [Label("Fall Speed"), Tooltip("How fast the player falls while Stomping.\nDefault: 28")]
     [DefaultValue(28), Range(1f, 100)]
     public float StompFallSpeed;
 
-    [Label("Number of Targets"), Tooltip("How many targets Stomp can damage at once.")]
+    [Label("Number of Targets"), Tooltip("How many targets Stomp can damage at once.\nDefault: 8")]
     [DefaultValue(8), Range(1, 32)]
     public int StompNumTargets;
 
     [Header("Charge Jump")]
 
-    [Label("Cooldown"), Tooltip("Cooldown of Charge Jump, in seconds.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Charge Jump, in seconds.\nDefault: 6")]
     [DefaultValue(6), Range(0f, 30)]
     public float CJumpCooldown;
 
-    [Label("Speed"), Tooltip("How fast Charge Jump moves the player.")]
+    [Label("Speed"), Tooltip("How fast Charge Jump moves the player.\nDefault: 1")]
     [DefaultValue(1), Range(0f, 10)]
     public float CJumpSpeedMultiplier;
 
     [Header("Wall Charge Jump")]
 
-    [Label("Cooldown"), Tooltip("Cooldown of Wall Charge Jump, in seconds.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Wall Charge Jump, in seconds.\nDefault: 4")]
     [DefaultValue(4), Range(0f, 30)]
     public float WCJumpCooldown;
 
-    [Label("Speed"), Tooltip("How fast Wall Charge Jump moves the player.")]
+    [Label("Speed"), Tooltip("How fast Wall Charge Jump moves the player.\nDefault: 1")]
     [DefaultValue(1), Range(0f, 10)]
     public float WCJumpSpeedMultipler;
 
-    [Label("Max Angle"), Tooltip("How far up or down Wall Charge Jump can be aimed.")]
+    [Label("Max Angle"), Tooltip("How far up or down Wall Charge Jump can be aimed.\nDefault: 0.65")]
     [DefaultValue(0.65f), Range(0.25f, (float)(Math.PI / 2))]
     public float WCJumpMaxAngle;
 
     [Header("Dash")]
 
-    [Label("Cooldown"), Tooltip("Cooldown of Dash, in seconds.\nNote this is only when a boss is alive.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Dash, in seconds.\nNote this is only when a boss is alive.\nDefault: 1")]
     [DefaultValue(1), Range(0f, 10)]
     public float DashCooldown;
 
-    [Label("Speed"), Tooltip("How fast Dash moves the player.")]
+    [Label("Speed"), Tooltip("How fast Dash moves the player.\nDefault: 1")]
     [DefaultValue(1), Range(0f, 10)]
     public float DashSpeedMultiplier;
 
     [Header("Charge Dash")]
     
-    [Label("Cooldown"), Tooltip("Cooldown of Charge Dash, in seconds.\nNote this is only when a boss is alive.")]
+    [Label("Cooldown"), Tooltip("Cooldown of Charge Dash, in seconds.\nNote this is only when a boss is alive.\nDefault: 3")]
     [DefaultValue(3), Range(0f, 30)]
     public float CDashCooldown;
 
-    [Label("Speed"), Tooltip("How fast Charge Dash moves the player.")]
+    [Label("Speed"), Tooltip("How fast Charge Dash moves the player.\nDefault: 1")]
     [DefaultValue(1), Range(0f, 10)]
     public float CDashSpeedMultiplier;
 
     [Header("Burrow")]
     
-    [Label("Strength"), Tooltip("Determines which tiles Burrow can dig through. Based on Pickaxe power.")]
+    [Label("Strength"), Tooltip("Determines which tiles Burrow can dig through. Based on Pickaxe power.\nDefault: 0")]
     [DefaultValue(0), Range(-1, 210)]
     public int BurrowStrength;
 
     public override void OnLoaded() {
       OriMod.ConfigAbilities = this;
     }
+    
     [OnDeserialized]
     public void OnDeserializedMethod(StreamingContext stream) {
       if (AirJumpCount < 0) AirJumpCount = 0;
