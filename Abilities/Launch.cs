880e291ca05f61c0f0246f9e641f90618918761a
TwiliChaos <zundisplayed@gmail.com>
2020-09-09 18:47:38 -0700

    *Ultra Launch*

--- Abilities/Launch.cs
+++ Abilities/Launch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using OriMod.Projectiles.Abilities;
 using OriMod.Utilities;
 using Terraria;
 using Terraria.ID;
@@ -17,7 +18,7 @@ namespace OriMod.Abilities {
   public sealed class Launch : Ability {
     internal Launch(AbilityManager manager) : base(manager) { }
     public override int Id => AbilityID.Launch;
-    public override byte Level => (byte)(abilities.chargeJump.Level >= 3 ? 1 : 0);
+    public override byte Level => (byte)Math.Max(0, abilities.chargeJump.Level - 2);
 
     /// <summary>
     /// Bash restrictions, plus in air and bash failed
@@ -25,10 +26,20 @@ namespace OriMod.Abilities {
     internal override bool CanUse => base.CanUse && Inactive && !oPlayer.IsGrounded && !abilities.stomp && !abilities.chargeJump && !abilities.bash && !abilities.climb;
     protected override int Cooldown => (int)(Config.BashCooldown * 30);
 
-    private int MinLaunchDuration => 15;
-    private int MaxLaunchDuration => 45;
-    private int EndDuration => 16;
-    private float LaunchSpeed => 15;
+    public ushort CurrentChain { get; private set; }
+    public ushort MaxChain {
+      get {
+        switch (Level) {
+          case 1: return 1;
+          case 2: return 3;
+          default: return (ushort)(Level + 1);
+        }
+      }
+    }
+    private int MinLaunchDuration => CurrentChain == 1 ? 15 : 20;
+    private int MaxLaunchDuration => CurrentChain == 1 ? 45 : 30;
+    private int EndDuration => CurrentChain == 1 ? 12 : 8;
+    private float LaunchSpeed => CurrentChain == 1 ? 20 : 30;
 
     private Vector2 startPos;
     public float launchAngle { get; private set; }
@@ -56,12 +67,14 @@ namespace OriMod.Abilities {
     protected override void UpdateUsing() {
       if (!Ending) {
         launchAngle = player.AngleTo(Main.MouseWorld);
-        player.velocity = Vector2.Zero;
+        player.velocity *= 0.86f;
         player.gravity = 0;
+        player.runSlowdown = 0;
       }
       if (IsLocal) {
         netUpdate = true;
       }
+      player.maxFallSpeed = LaunchSpeed;
       // Allow only quick heal and quick mana
       player.controlJump = false;
       player.controlUp = false;
@@ -98,13 +111,16 @@ namespace OriMod.Abilities {
     }
 
     protected override void UpdateEnding() {
+      if (CurrentTime == 0) {
+        NewAbilityProjectile<LaunchProjectile>(damage: 70);
+      }
+      player.pulley = false;
+      player.immuneTime = 5;
       player.velocity = launchDirection * LaunchSpeed;
     }
 
     private void End() {
-      player.pulley = false;
-      player.velocity = launchDirection * 15;
-      player.immuneTime = 5;
+      player.velocity = launchDirection * 10;
       oPlayer.UnrestrictedMovement = true;
       oPlayer.PlayNewSound("Ori/Bash/seinBashEnd" + rand.NextNoRepeat(3), 0.5f);
       Refreshed = false;
@@ -115,12 +131,21 @@ namespace OriMod.Abilities {
         startPos = player.Center;
         oPlayer.PlayNewSound("Ori/Bash/seinBashStartA", 0.5f, localOnly: true);
         SetState(State.Starting);
+        CurrentChain = 1;
         return;
       }
       else if (InUse) {
+        if (oPlayer.IsGrounded || oPlayer.OnWall) {
+          // Prevent any usage of Launch while not in air
+          SetState(State.Inactive);
+          return;
+        }
         if (Starting) {
           if (CurrentTime > MinLaunchDuration) {
             SetState(State.Active, preserveCurrentTime: true);
+          }
+          else if (CurrentChain > 1 && !OriMod.BashKey.Current) {
+            SetState(State.Inactive);
           }
           return;
         }
@@ -129,14 +154,24 @@ namespace OriMod.Abilities {
             oPlayer.PlayNewSound("Ori/Bash/seinBashLoopA", 0.5f, localOnly: true);
           }
           if (CurrentTime > MaxLaunchDuration || IsLocal && !OriMod.BashKey.Current) {
-            End();
             SetState(State.Ending);
           }
           return;
         }
         if (Ending) {
-          if (CurrentTime > EndDuration) {
+          if (oPlayer.IsGrounded || oPlayer.OnWall) {
             SetState(State.Inactive);
+          }
+          // Post-ending state depends on player input
+          if (IsLocal && CurrentTime > EndDuration) {
+            if (CurrentChain < MaxChain && OriMod.BashKey.Current) {
+              CurrentChain++;
+              SetState(State.Starting);
+            }
+            else {
+              End();
+              SetState(State.Inactive);
+            }
           }
         }
       }
@@ -148,6 +183,7 @@ namespace OriMod.Abilities {
     protected override void TickCooldown() {
       if (oPlayer.IsGrounded || oPlayer.OnWall || abilities.bash) {
         Refreshed = true;
+        CurrentChain = 0;
       }
     }
   }
---
+++ Projectiles/Abilities/LaunchProjectile.cs
@@ -0,0 +1,24 @@
+﻿using OriMod.Abilities;
+
+namespace OriMod.Projectiles.Abilities {
+  /// <summary>
+  /// Projectile hitbox for when the player is using <see cref="Launch"/>.
+  /// <para>Ending stomp spawns a <see cref="StompEnd"/> projectile to deal damage.</para>
+  /// </summary>
+  public sealed class LaunchProjectile : AbilityProjectile {
+    public override byte abilityID => AbilityID.Launch;
+
+    public override void SetDefaults() {
+      base.SetDefaults();
+      projectile.width = 56;
+      projectile.height = 56;
+    }
+
+    public override void CheckAbilityActive() {
+      if (ability.Ending) {
+        projectile.timeLeft = 10;
+      }
+    }
+  }
+}
+
