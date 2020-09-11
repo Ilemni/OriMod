2bf3c292913b744b8de98c964d48e5839fcee6ac
TwiliChaos <zundisplayed@gmail.com>
2020-09-10 21:15:27 -0700

    Maybe hit compile first?

--- Items/Abilities/ChargeJumpLevel2.cs
+++ Items/Abilities/ChargeJumpLevel2.cs
@@ -10,7 +10,7 @@ namespace OriMod.Items.Abilities {
 
     public override void AddRecipes() {
       var recipe = GetAbilityRecipe<ChargeJumpLevel1>();
-      recipe.AddRecipeGroup(ItemID.ChlorophyteBar, 22);
+      recipe.AddIngredient(ItemID.ChlorophyteBar, 22);
       recipe.AddIngredient(ItemID.SoulofFlight, 15);
       recipe.AddRecipe();
     }
