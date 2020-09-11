d7c6905ad18c1ee1661638b3fcb8c5a05318c8c7
TwiliChaos <zundisplayed@gmail.com>
2020-09-11 16:06:21 -0700

    Revert "Update AnimLib... I kinda hate this boilerplace code rn"
    This reverts commit 50374cb50ecc63b0b6ebdd6f0bd130834fb76d2c.
    See commit TwiliChaos/AnimLib@e21638514420288ec8547806da57e79d2e5626c6

--- Animations/OriAnimationSources.cs
+++ Animations/OriAnimationSources.cs
@@ -9,91 +9,90 @@ namespace OriMod.Animations {
   public sealed class PlayerAnim : AnimationSource {
     public override PointByte spriteSize { get; } = new PointByte(64, 68);
 
-    public override Dictionary<string, Track> tracks => _tracks ?? (_tracks = new Dictionary<string, Track> {
-      ["Default"] = Single(
+    public override Dictionary<string, Track> tracks { get; } = new Dictionary<string, Track> {
+      ["Default"] = Track.Single(
           F(0, 0)
         ),
-      ["Idle"] = Range(
+      ["Idle"] = Track.Range(
           F(0, 1, 9), F(0, 8, 9)
         ),
-      ["IdleAgainst"] = Range(
+      ["IdleAgainst"] = Track.Range(
           F(0, 9, 7), F(0, 14, 7)
         ),
-      ["LookUpStart"] = Single(
+      ["LookUpStart"] = Track.Single(
           F(1, 0)
         ),
-      ["LookUp"] = Range(
+      ["LookUp"] = Track.Range(
           F(1, 1, 8), F(1, 7, 8)
         ),
-      ["CrouchStart"] = Single(
+      ["CrouchStart"] = Track.Single(
           F(1, 8)
         ),
-      ["Crouch"] = Single(
+      ["Crouch"] = Track.Single(
           F(1, 9)
         ),
-      ["Running"] = Range(
+      ["Running"] = Track.Range(
           F(2, 0, 4), F(2, 10, 4)
         ),
-      ["Dash"] = Range(
+      ["Dash"] = Track.Range(
           F(2, 11, 36), F(2, 12, 12)
         ),
-      ["Bash"] = new Track(this, new Frame[] {
+      ["Bash"] = new Track(new Frame[] {
           F(2, 13, 40), F(2, 12)
       }),
-      ["AirJump"] = Single(
+      ["AirJump"] = Track.Single(
           F(3, 0, 32)
         ),
-      ["Jump"] = new Track(this, new Frame[] {
+      ["Jump"] = new Track(new Frame[] {
           F(3, 2, 14), F(3, 1)
         }),
-      ["IntoJumpBall"] = Range(LoopMode.None,
+      ["IntoJumpBall"] = Track.Range(LoopMode.None,
           F(3, 3, 6), F(3, 4, 4)
         ),
-      ["ChargeJump"] = Range(LoopMode.None, Direction.PingPong,
+      ["ChargeJump"] = Track.Range(LoopMode.None, Direction.PingPong,
           F(3, 5, 4), F(3, 8, 4)
         ),
-      ["Falling"] = Range(
+      ["Falling"] = Track.Range(
           F(3, 9, 4), F(3, 12, 4)
         ),
-      ["FallNoAnim"] = Single(
+      ["FallNoAnim"] = Track.Single(
           F(3, 13)
         ),
-      ["GlideStart"] = Range(LoopMode.None,
+      ["GlideStart"] = Track.Range(LoopMode.None,
           F(4, 0, 5), F(4, 2, 5)
         ),
-      ["GlideIdle"] = Single(
+      ["GlideIdle"] = Track.Single(
           F(4, 3)
         ),
-      ["Glide"] = Range(
+      ["Glide"] = Track.Range(
           F(4, 4, 5), F(4, 9, 5)
         ),
-      ["ClimbIdle"] = Single(
+      ["ClimbIdle"] = Track.Single(
           F(5, 0)
         ),
-      ["Climb"] = Range(
+      ["Climb"] = Track.Range(
           F(5, 1, 4), F(5, 8, 4)
         ),
-      ["WallSlide"] = Range(
+      ["WallSlide"] = Track.Range(
           F(5, 9, 5), F(5, 12, 5)
         ),
-      ["WallJump"] = Single(
+      ["WallJump"] = Track.Single(
           F(5, 13, 12)
         ),
-      ["WallChargeJumpCharge"] = new Track(this, new Frame[] {
+      ["WallChargeJumpCharge"] = new Track(new Frame[] {
           F(6, 0, 16), F(6, 1, 10), F(6, 2)
         }),
-      ["WallChargeJumpAim"] = Range(
+      ["WallChargeJumpAim"] = Track.Range(
           F(6, 2), F(6, 6) // No duration, frames are selected by code, i.e. player mouse position
         ),
-      ["Burrow"] = Range(
+      ["Burrow"] = Track.Range(
           F(7, 0, 3), F(7, 7, 3)
         ),
-      ["Transform"] = new Track(this, new IFrame[] {
+      ["Transform"] = new Track(new IFrame[] {
           F("OriMod/Animations/TransformAnim", 0, 0, 2), F(0, 1, 60), F(0, 2, 60), F(0, 3, 120), F(0, 4, 40), F(0, 5, 40), F(0, 6, 40), F(0, 7, 30),
           F("OriMod/Animations/PlayerAnim", 6, 7, 6), F(6, 8, 50), F(6, 9, 6), F(6, 10, 60), F(6, 11, 10), F(6, 12, 40), F(6, 13, 3), F(6, 14, 60)
         }),
-    });
-    private Dictionary<string, Track> _tracks;
+    };
 
     public static PlayerAnim Instance => _instance ?? (_instance = GetInstance());
     private static PlayerAnim _instance;
@@ -110,12 +109,11 @@ namespace OriMod.Animations {
   public sealed class BashAnim : AnimationSource {
     public override PointByte spriteSize { get; } = new PointByte(152, 20);
 
-    Dictionary<string, Track> _tracks;
-    public override Dictionary<string, Track> tracks => _tracks ?? (_tracks = new Dictionary<string, Track> {
-      ["Bash"] = Range(
+    public override Dictionary<string, Track> tracks { get; } = new Dictionary<string, Track> {
+      ["Bash"] = Track.Range(
         F(0, 0), F(0, 2)
       )
-    });
+    };
   }
 
   /// <summary>
@@ -124,17 +122,16 @@ namespace OriMod.Animations {
   public sealed class GlideAnim : AnimationSource {
     public override PointByte spriteSize { get; } = new PointByte(128, 128);
 
-    private Dictionary<string, Track> _tracks;
-    public override Dictionary<string, Track> tracks => _tracks ?? (_tracks = new Dictionary<string, Track> {
-      ["GlideStart"] = Range(LoopMode.None,
+    public override Dictionary<string, Track> tracks { get; } = new Dictionary<string, Track> {
+      ["GlideStart"] = Track.Range(LoopMode.None,
         F(0, 0, 5), F(0, 2, 5)
       ),
-      ["GlideIdle"] = Single(
+      ["GlideIdle"] = Track.Single(
         F(0, 3)
       ),
-      ["Glide"] = Range(
+      ["Glide"] = Track.Range(
         F(0, 4, 5), F(0, 9, 5)
       ),
-    });
+    };
   }
 }
