6c78b53fcbb48da8f3ffe0ab93085af448caa65f
TwiliChaos <zundisplayed@gmail.com>
2020-09-25 15:57:33 -0700

    Texture organization, remove unused textures

--- OriTextures.cs
+++ OriTextures.cs
@@ -6,10 +6,10 @@ namespace OriMod {
     private OriTextures() { }
 
     internal readonly ReferencedTexture2D PlayerPrimary = new ReferencedTexture2D("Animations/PlayerAnim");
-    internal readonly ReferencedTexture2D PlayerSecondary = new ReferencedTexture2D("PlayerEffects/OriPlayerSecondary");
+    internal readonly ReferencedTexture2D PlayerSecondary = new ReferencedTexture2D("Animations/PlayerAnimSecondary");
     internal readonly ReferencedTexture2D Transform = new ReferencedTexture2D("Animations/TransformAnim");
-    internal readonly ReferencedTexture2D Trail = new ReferencedTexture2D("PlayerEffects/OriGlow");
-    [System.Obsolete] internal readonly ReferencedTexture2D SoulLink = new ReferencedTexture2D("PlayerEffects/RevSoulLinkSpritesheet");
+    internal readonly ReferencedTexture2D Trail = new ReferencedTexture2D("PlayerEffects/PlayerTrail");
+    [System.Obsolete] internal readonly ReferencedTexture2D SoulLink; // = new ReferencedTexture2D("PlayerEffects/RevSoulLinkSpritesheet");
     internal readonly ReferencedTexture2D BurrowTimer = new ReferencedTexture2D("PlayerEffects/BurrowTimer");
     internal readonly ReferencedTexture2D BashArrow = new ReferencedTexture2D("Animations/BashAnim");
     internal readonly ReferencedTexture2D Feather = new ReferencedTexture2D("Animations/GlideAnim");
