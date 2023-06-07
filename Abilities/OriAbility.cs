ede13618493e74dfd94dd720d28a59c8fe124697
TwiliChaos <accounts+github@ilemni.com>
2023-06-07 02:35:29 -0700

    Lazy load OriAbility properties Input was null during Initialize()

--- Abilities/OriAbility.cs
+++ Abilities/OriAbility.cs
@@ -3,14 +3,11 @@ using AnimLib.Abilities;
 namespace OriMod.Abilities;
 
 public abstract class OriAbility : Ability<OriAbilityManager> {
-  public override void Initialize() {
-    base.Initialize();
-    oPlayer = abilities.oPlayer;
-    input = oPlayer.input;
-  }
-  
-  protected OriPlayer oPlayer { get; private set; }
-  protected OriInput input { get; private set; }
+
+  protected OriPlayer oPlayer => _oPlayer ??= abilities.oPlayer;
+  protected OriInput input => _input ??= oPlayer.input;
+  private OriPlayer _oPlayer;
+  private OriInput _input;
 
   protected bool OnWall => oPlayer.OnWall;
   protected bool IsGrounded => oPlayer.IsGrounded;
@@ -18,6 +15,6 @@ public abstract class OriAbility : Ability<OriAbilityManager> {
   protected void PlaySound(string path, float volume = 1, float pitch = 0)
     => oPlayer.PlaySound(path, volume, pitch);
 
-    protected void PlayLocalSound(string path, float volume = 1, float pitch = 0)
-        => oPlayer.PlayLocalSound(path, volume, pitch);
+  protected void PlayLocalSound(string path, float volume = 1, float pitch = 0)
+    => oPlayer.PlayLocalSound(path, volume, pitch);
 }
