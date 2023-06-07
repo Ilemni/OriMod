1e116dd97dec052567e5dd06fbf4c110ccf33e02
TwiliChaos <accounts+github@ilemni.com>
2023-06-07 02:34:30 -0700

    OriMod.Call rewrite

--- OriModCall.cs
+++ OriModCall.cs
@@ -1,28 +1,54 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 
 namespace OriMod;
 
 public static class OriModCall {
+  private const string ResetPlayerCmd = "ResetPlayerModData";
+  private const string IsOriCmd = "IsOri";
+
   /// <summary>
-  /// Interact with <see cref="OriMod"/> using various inputs.
+  /// Interact with <see cref="OriMod"/> using various inputs.<br />
+  /// If any arguments are invalid, the call will warn and return <see langword="null"/>.
   /// <list type="table">
   /// <listheader>
   /// <term>Command/Parameters</term>
   /// <description>Description</description>
   /// </listheader>
   /// <item>
-  /// <term>"ResetPlayerModData"</term>
+  /// <term>
+  /// "ResetPlayerModData"<br />
+  /// <see cref="Player"/>, <see cref="ModPlayer"/>, or <see cref="int"/>
+  /// </term>
   /// <description>
-  /// Arguments <see cref="Player"/> -or- <see cref="ModPlayer"/>, resets the <see cref="OriPlayer"/> data on the given <see cref="Player"/>/<see cref="ModPlayer"/> (set-only) —
-  /// Returns <see langword="true"/> if arguments are valid; otherwise, <see langword="false"/>.
+  /// (Method call)<br />
+  /// Resets the <see cref="OriPlayer"/> data on the given <see cref="Player"/>/<see cref="ModPlayer"/><br />
+  /// Returns <see langword="null"/>
   /// </description>
   /// </item>
   /// <item>
-  /// <term>"IsOri"</term>
+  /// <term>
+  /// "IsOri"<br />
+  /// <see cref="Player"/>, <see cref="ModPlayer"/>, or <see cref="int"/><br />
+  /// </term>
   /// <description>
-  /// Arguments <see cref="Player"/> -or- <see cref="ModPlayer"/>, checks if the player is in Ori state (readonly) —
-  /// Returns <see langword="true"/> if the player is Ori or transforming into Ori; <see langword="false"/> if neither; or <see langword="null"/> if arguments are invalid.
+  /// (Get)<br />
+  /// Checks if the given <see cref="OriPlayer"/> is in Ori state.<br />
+  /// Returns <see langword="bool"/>:<br />
+  /// <see langword="true"/> if the player is Ori, or transforming into Ori;<br />
+  /// <see langword="false"/> if neither;<br />
+  /// </description>
+  /// </item>
+  /// <item>
+  /// <term>
+  /// "IsOri"<br />
+  /// <see cref="Player"/>, <see cref="ModPlayer"/>, or <see cref="int"/><br />
+  /// <see cref="bool"/>
+  /// </term>
+  /// <description>
+  /// (Set)<br />
+  /// Sets the player's Ori state to the given value.<br />
+  /// Returns <see langword="null"/>
   /// </description>
   /// </item>
   /// </list>
@@ -30,32 +56,48 @@ public static class OriModCall {
   public static object Call(params object[] args) {
     int len = args.Length;
     if (len <= 0 || args[0] is not string cmd) return null;
-      
-    OriPlayer oPlayer = null;
-    if (len >= 2) {
-      oPlayer = GetOriPlayer(args[1]);
-    }
+    
+    OriPlayer oPlayer = len >= 2 ? GetOriPlayer(args[1]) : null;
 
     switch (cmd) {
-      case "ResetPlayerModData":
-        if (oPlayer is not null) {
-          oPlayer.ResetData();
-          return true;
-        }
-        OriMod.Log.Warn($"{OriMod.instance.Name}.Call() - ResetPlayerModData - Expected type {typeof(Player)}, got {args[1].GetType()}");
-        return false;
-      case "IsOri":
-        if (oPlayer is not null) {
-          return oPlayer.IsOri || oPlayer.Transforming;
-        }
-        OriMod.Log.Warn($"{OriMod.instance.Name}.Call() - Transforming - Expected type {typeof(Player)}, got {args[1].GetType()}");
-        return null;
+      case ResetPlayerCmd when oPlayer is not null:
+        // ("ResetPlayerModData", player)
+        oPlayer.ResetData();
+        break;
+      case ResetPlayerCmd:
+        // ("ResetPlayerModData", ???)
+        WarnArgType(ResetPlayerCmd, 1, args[1], typeof(Player));
+        break;
+      case IsOriCmd when oPlayer is not null && len < 3:
+        // ("IsOri", player)
+        return oPlayer.IsOri || oPlayer.Transforming;
+      case IsOriCmd when oPlayer is not null && args[2] is bool isOri:
+        // ("IsOri", player, isOri)
+        oPlayer.IsOri = isOri;
+        break;
+      case IsOriCmd when oPlayer is not null:
+        // ("IsOri", player, ???)
+        WarnArgType(IsOriCmd, 2, args[2], typeof(bool));
+        break;
+      case IsOriCmd:
+        // ("IsOri", ???)
+        WarnArgType(IsOriCmd, 1, args[1], typeof(Player));
+        break;
     }
     return null;
   }
 
   private static OriPlayer GetOriPlayer(object obj) {
-    Player player = obj is Player p ? p : obj is ModPlayer modPlayer ? modPlayer.Player : null;
+    Player player = obj switch {
+      Player p => p,
+      ModPlayer modPlayer => modPlayer.Player,
+      int index => Main.player[index],
+      _ => null
+    };
     return player?.GetModPlayer<OriPlayer>();
+  }
+
+  private static void WarnArgType(string cmd, int argIndex, object arg, System.Type expectedType) {
+    OriMod.Log.Warn($"{OriMod.instance.Name}.Call() - {cmd} - Arg {argIndex}: Expected type {expectedType}, got {arg.GetType()}");
   }
 }
