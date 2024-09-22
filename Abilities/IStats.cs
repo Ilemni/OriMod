f5bdb31336f426150790c357cb262bdae3fbfa5c
TwiliChaos <accounts+github@ilemni.com>
2024-09-22 12:58:23 -0700

    Fix an out of bounds, add max size

--- Abilities/IStats.cs
+++ Abilities/IStats.cs
@@ -3,6 +3,12 @@
 namespace OriMod.Abilities;
 
 internal interface IStats<T> where T : IStats<T> {
+  /// <summary>
+  /// Array of stat values, indexed by level.
+  /// </summary>
+  /// <remarks>
+  /// This returns by ref so that <see cref="Get"/> can resize it if needed.
+  /// </remarks>
   protected static abstract ref T[] Values { get; }
 
   /// <summary>
@@ -10,16 +16,29 @@ internal interface IStats<T> where T : IStats<T> {
   /// </summary>
   protected static abstract T CreateFromLevel(int level);
 
+  public static int MaxSize => 128;
+
+  /// <summary>
+  /// Gets the value of <see cref="Values"/> at the specified index.
+  /// If the specified index is larger than the value, the array will be resized to accomodate it.
+  /// </summary>
+  /// <param name="index"></param>
+  /// <returns></returns>
+  /// <exception cref="ArgumentOutOfRangeException">
+  /// <paramref name="index"/> was negative or greater than <see cref="MaxSize"/>
+  /// </exception>
   public static ref T Get(int index) {
     ArgumentOutOfRangeException.ThrowIfNegative(index);
+    ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, MaxSize);
+
     ref var values = ref T.Values;
     int oldLen = values.Length;
     if (index < oldLen) {
       return ref values[index];
     }
 
-    Array.Resize(ref values, index);
-    for (int i = oldLen; i < index; i++) {
+    Array.Resize(ref values, index + 1);
+    for (int i = oldLen; i <= index; i++) {
       values[i] = T.CreateFromLevel(i);
     }
 
