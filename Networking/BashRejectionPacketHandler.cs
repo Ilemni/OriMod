ddfaa46cbab31c5d6955df4ab3c1df9984697f0f
TwiliChaos <accounts+github@ilemni.com>
2024-11-21 20:23:41 -0800

    Add BashRejection packet type

---
+++ Networking/BashRejectionPacketHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using OriMod.Abilities;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace OriMod.Networking;
+
+/// <summary>
+/// Sends from the server only, to a player that has <see cref="Bash"/>ed an NPC,
+/// that the bash conditions are invalid and <see cref="Bash"/> must be cancelled.
+/// For example, a Bash attempt may cancel if two players attempt to bash the same NPC at the same time.
+/// </summary>
+/// <param name="handlerType"></param>
+/// <remarks>
+/// The packet doesn't contain any data specific to the handler, only its type.
+/// </remarks>
+internal class BashRejectionPacketHandler(byte handlerType) : PacketHandler(handlerType) {
+  internal override void HandlePacket(BinaryReader reader, int fromWho) {
+    // Packet is always sent from server, telling the local player that its bash attempt is invalid.
+    OriPlayer oPlayer = Main.LocalPlayer.GetModPlayer<OriPlayer>();
+    if (oPlayer.ActiveState is Bash) {
+      oPlayer.Character.TriggerState<NoAbility>();
+    }
+  }
+
+  internal void SendPacket(int toWho) {
+    if (!Main.dedServ) {
+      throw new InvalidOperationException("Cannot send \"Bash Rejection\" packet from client.");
+    }
+
+    ModPacket packet = GetPacket(fromWho: 255);
+    packet.Send(toWho);
+  }
+}
--- Networking/ModNetHandler.cs
+++ Networking/ModNetHandler.cs
@@ -11,19 +11,29 @@ namespace OriMod.Networking;
 internal sealed class ModNetHandler : ModSystem {
   public override void SetStaticDefaults() {
     OriPlayerHandler = new OriPlayerPacketHandler(OriState);
+    BashRejection = new BashRejectionPacketHandler(BashRejectType);
   }
 
   public override void Unload() {
     OriPlayerHandler = null!;
+    BashRejection = null!;
   }
 
   /// <summary>
-  /// Type for <see cref="OriPlayerPacketHandler"/>.
+  /// Enum value for <see cref="OriPlayerPacketHandler"/>.
   /// </summary>
   private const byte OriState = 1;
 
+  /// <summary>
+  /// Enum value for <see cref="BashRejectionPacketHandler"/>
+  /// </summary>
+  private const byte BashRejectType = 2;
+
   /// <inheritdoc cref="OriPlayerPacketHandler"/>
   internal static OriPlayerPacketHandler OriPlayerHandler { get; private set; } = null!; // SetStaticDefaults()
+
+  /// <inheritdoc cref="BashRejectionPacketHandler"/>
+  internal static BashRejectionPacketHandler BashRejection { get; private set; } = null!; // SetStaticDefaults()
 
   /// <summary>
   /// Sends the received <see cref="ModPacket"/> to the desired <see cref="PacketHandler"/> based on data read from <paramref name="reader"/>.
@@ -34,6 +44,7 @@ internal sealed class ModNetHandler : ModSystem {
     byte packetClass = reader.ReadByte();
     PacketHandler? handler = packetClass switch {
       OriState => OriPlayerHandler,
+      BashRejectType => BashRejection,
       _ => null,
     };
 
